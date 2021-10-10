6997cb4c6b05387c0cb08fe2e9ab250a50602ba6
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2021-10-10 02:15:19 +0200

    add js

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto,CA&appid=bd83f143dfb68bc132336480ad98d6fd")
-    .then(res => res.json())
-    .then(res => {
-        const temp = +(res.main.temp - 273.15).toFixed(1);
-        const tempMax = +(res.main.temp_max - 273.15).toFixed(1);
-        const tempMin = +(res.main.temp_min - 273.15).toFixed(1);
-        const tempFeels = +(res.main.feels_like - 273.15).toFixed(1);
-        const name = res.name;
-        const humidity = res.main.humidity;
-        const weatherDesc = res.weather[0].description;
-        const weatherMain = res.weather[0].main;
-        const wind = res.wind.speed
-        console.log(weatherDesc);
-        console.log(res);
+const searchInput = document.querySelector('.weather-search__input');
+const searchIcon = document.querySelector('.weather-search__icon');
+const locationCity = document.querySelector('.location-city');
+const weatherDescription = document.querySelector('.description');
+const temperature = document.querySelector('.temperature');
+const windSpeed = document.querySelector('.wind-speed');
+const humidity = document.querySelector('.air-humidity');
+const icon = document.querySelector('.icon');
 
-    })
-    .catch(error => console.log("Błąd: ", error))
+const getWeatherData = function (city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bd83f143dfb68bc132336480ad98d6fd`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setInformation(data);
+        })
+}
+
+const setInformation = function (data) {
+    locationCity.textContent = data.name;
+    weatherDescription.textContent = data.weather[0].description.toUpperCase();
+    windSpeed.textContent = (data.wind.speed).toFixed() + ' mph';
+    humidity.textContent = data.main.humidity + '%';
+    icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    setTemperature(data.main.temp);
+
+}
+
+const setTemperature = function (temp) {
+    temperature.textContent = '';
+    temperature.insertAdjacentHTML('beforeend', `${+(temp - 273.15).toFixed()}<span>&#8451;</span>`);
+}
+
+const getInput = function (e) {
+    e.preventDefault();
+    const input = searchInput.value;
+    getWeatherData(input);
+}
+
+searchIcon.addEventListener('click', getInput)
+window.addEventListener('keyup', function (e) {
+    if (e.keyCode == 13) {
+        getInput(e)
+    } else return
+})
 
 
-// 1. Temperatura w C i F 
-// 2. Wilgotność 
-
+console.log(icon.src);
